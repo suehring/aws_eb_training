0c7275e56fbcafea3d13e278466f1e31164be740
Steve Suehring <suehring@braingia.org>
2017-10-20 08:23:25 -0500

    Cleanup

--- php/worker/workerapp.php
+++ php/worker/workerapp.php
@@ -1,10 +1,7 @@
 <?
 if ($_SERVER['REQUEST_METHOD'] === 'POST')
 {
-  /*$file = '/tmp/emailrequest.log';
-  $message = file_get_contents('php://input');
-  file_put_contents($file, date('Y-m-d H:i:s') . " Received message: " . $message . "\n", FILE_APPEND);*/
-  $to = "suehring@braingia.org";
+  $to = "you@example.org";
 	$subject = "Test";
 	$message = "This is a test message";
 	mail($to,$subject,$message);
